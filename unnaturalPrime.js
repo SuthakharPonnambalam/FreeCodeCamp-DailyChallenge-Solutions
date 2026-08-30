3eccd60567c3aeabc3d72ad2f2d0774d73aa1149
Suthakhar <suthakhar.ponnambalam@gmail.com>
2026-08-29 22:00:19 -0400

    2 more problems added

---
+++ unnaturalPrime.js
@@ -0,0 +1,26 @@
+//Date: August 29, 2026
+
+/*
+Given an integer, determine if that number is a prime number or a negative prime number.
+A prime number is a positive integer greater than 1 that is only divisible by 1 and itself.
+A negative prime number is the negative version of a positive prime number.
+1 and 0 are not considered prime numbers.
+*/
+
+function isUnnaturalPrime(n) {
+    if(n === 0 || n === 1 || n === -1){
+        return false;
+    }
+    if(n < 0){
+        n = Math.abs(n);
+    }
+    for(let i = 2; i <= Math.floor(n/2); i++){
+        if(n%i === 0){
+            return false
+        }
+    }
+    return true;
+}
+
+console.log(isUnnaturalPrime(5));
+console.log(isUnnaturalPrime(-5));
